5cbb5a00f7e2879aed83ec559827fa2bdb641b26
zed <yakdi.zak@gmail.com>
2020-02-25 13:44:55 +0100

    :recycle: Convert class to functional component

--- client/src/components/NavigationBar/NavigationBar.js
+++ client/src/components/NavigationBar/NavigationBar.js
@@ -1,6 +1,6 @@
 // Navigation bar of the website
 
-import React, { Component } from 'react';
+import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import './NavigationBar.css';
 
@@ -21,27 +21,5 @@ const navigationBar = () => {
         </div>
     )
 }
-
-/*class NavigationBar extends Component {
-    state = {};
-
-    render() {
-        return (
-            <div className="navigationBar">
-                <Navbar expand="lg" variant="dark">
-                    <Navbar.Brand href="/">Zak-Market</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/products">Products</Nav.Link>
-                            <Nav.Link href="/createProduct">Create a product</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            </div>
-        );
-    }
-}*/
 
 export default navigationBar;
