bde0028893afbaff3ae262aa045de0d4e51c6f64
zed <yakdi.zak@gmail.com>
2020-02-25 19:26:36 +0100

    :white_check_mark: Add rendering tests for Product and Products components

--- client/src/components/Product/__tests__/Product.test.js
+++ client/src/components/Product/__tests__/Product.test.js
@@ -1,21 +1,40 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import Product from '../../Product/Product';
+import Product from '../Product';
+import { act } from "react-dom/test-utils";
+import { render, unmountComponentAtNode } from "react-dom";
 
-describe('Product', () => {
-  const product = {
-    idProduct: 12,
-    name: 'Test product',
-    price: 22,
-    details: 'Details of the test product',
-    type: 'test'
-  };
 
-  const handleDelete = jest.fn();
 
-  test('snapshot renders', () => {
-    const component = renderer.create(<Product product={product} handleDelete={handleDelete}/>);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+describe('Product display', () => {
+  let container = null;
+
+  beforeEach(() => {
+    // Put a div element in document to test inside
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    // Remove div element from document
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  test("displays information about the product", () => {
+
+    const product = {
+      idProduct: 18,
+      name: "Sweet name",
+      price: 999,
+      type: "Nice type",
+      details: "This is a test and these are the details"
+    };
+
+    act(() => {
+      render(<Product product={product}/>, container);
+    });
+    expect(container.textContent).toBe("NICE TYPESweet name999 $Details: This is a test and these are the detailsUpdateDelete");
+
   });
 });
--- client/src/components/Products/__tests__/Products.test.js
+++ client/src/components/Products/__tests__/Products.test.js
@@ -1,11 +1,67 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import Products from '../Products';
+import { act } from "react-dom/test-utils";
+import { render, unmountComponentAtNode } from "react-dom";
 
-describe('Products', () => {
-  test('snapshot renders', () => {
-    const component = renderer.create(<Products />);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+
+
+describe('Fetch products', () => {
+  let container = null;
+  beforeEach(() => {
+    // Put a div element in document to test inside
+    container = document.createElement("div");
+    document.body.appendChild(container);
   });
+
+  afterEach(() => {
+    // Remove div element from document
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  test("shows a list of products", async () => {
+    const fakeResponse = [
+      {
+        "idProduct": 0,
+        "name": "Macbook Pro 13 inches",
+        "price": 1100,
+        "type": "computer",
+        "details": "Manufactured in 2019, Qwerty keyboard, touchbar"
+      },
+      {
+        "idProduct": 1,
+        "name": "Benjamin Franklin by Walter Isaacson",
+        "price": 15.5,
+        "type": "book",
+        "details": "Paperback, 586 pages, very nice book"
+      },
+      {
+        "idProduct": 2,
+        "name": "Skateboard 8 inches",
+        "price": 80,
+        "type": "Skateboard",
+        "details": "Damaged grip tape, spitfire wheels and Venture trucks"
+      }
+    ];
+
+    jest.spyOn(window, "fetch").mockImplementation(() => {
+      const fetchResponse = {
+        json: () => Promise.resolve(fakeResponse)
+      };
+      return Promise.resolve(fetchResponse);
+    });
+
+    await act(async () => {
+      render(<Products />, container);
+    });
+
+    expect(container.querySelectorAll("div.flexItem")[0]).toBeDefined();
+    expect(container.querySelectorAll("div.flexItem")[1]).toBeDefined();
+    expect(container.querySelectorAll("div.flexItem")[2]).toBeDefined();
+    expect(container.querySelectorAll("div.flexItem").length).toEqual(3);
+
+    window.fetch.mockRestore();
+  });
+
 });
